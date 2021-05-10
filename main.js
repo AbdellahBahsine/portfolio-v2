ee3d6c72fbf4020d75315325d2b16e118331da85
Paylo01 <paylo_@outlook.com>
2021-05-10 07:43:36 +0000

    create about, my work, blogs sections

--- main.js
+++ main.js
@@ -6,3 +6,42 @@ bars.addEventListener('click', () => {
     barsContainer.classList.toggle('open')
     mobileMenu.classList.toggle('open')
 })
+
+let typeText = document.querySelector("#typeText")
+let textToBeTyped = "Abdellah"
+let index = 0, isAdding = true 
+
+function playAnim() {
+  setTimeout(function () {
+
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      
+      if (index > textToBeTyped.length) {
+        
+        isAdding = false
+        
+        setTimeout( function () {
+          playAnim()
+        }, 2000)
+        return
+      } else {
+        
+        index++
+      }
+    } else {
+      
+      if (index === 0) {
+        
+        isAdding = true
+      } else {
+        
+        index--
+      }
+    }
+    
+    playAnim()
+  }, 120)
+}
+
+playAnim()
